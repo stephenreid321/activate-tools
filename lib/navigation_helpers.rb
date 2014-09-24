2c557f6b4c016ba9d5919196d272c66948737201
Stephen Reid <postscript07@gmail.com>
2014-09-24 15:33:11 +0100

    .

--- lib/navigation_helpers.rb
+++ lib/navigation_helpers.rb
@@ -1,20 +1,21 @@
 module Activate
   module NavigationHelpers
       
-    def ul_nav(css_class, items)
+    def ul_nav(css_class, items, prefix: '')
       s = ''
       s << %Q{<ul class="#{css_class}">}      
-      items.each { |name, path|
+      items.each { |name, path, icon|
+        icon = %Q{<i class="#{icon}"></i>} if icon
         if path.is_a? Array
           s << %Q{<li class="dropdown">}
-          s << %Q{<a data-toggle="dropdown" class="dropdown-toggle" href="javascript:;">#{name} <b class="caret"></b></a>}
+          s << %Q{<a data-toggle="dropdown" class="dropdown-toggle" href="javascript:;">#{icon} #{name} <b class="caret"></b></a>}
           s << ul_nav('dropdown-menu', path)
           s << %Q{</li>}
         else
           s << %Q{<li}
           s << %Q{ class="active" } if request.path == path
           s << %Q{>}
-          s << %Q{<a href="#{path}">#{name}</a>}
+          s << %Q{<a href="#{path}">#{icon} #{name}</a>}
           s << %Q{</li>}
         end
       }

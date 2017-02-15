551d2c7026a1e9eeb0763c002ecc5d96d8fa0343
Stephen Reid <postscript07@gmail.com>
2017-02-15 22:16:42 +0000

    .

--- lib/datetime_helpers.rb
+++ lib/datetime_helpers.rb
@@ -3,16 +3,14 @@ module Activate
      
     def date_select_tags(name, options={})
       value = options[:value]
-      start_year = options[:start_year] ? options[:start_year].to_i : (value ? [Time.zone.now.year,value.year].min : Time.zone.now.year)
-      end_year = options[:end_year] ? options[:end_year].to_i : start_year+7
       disabled = options[:disabled] || false
       required = options[:required] || false
       # options[:class]
       s = []
       s << '<span class="date">'
-      s << select_tag(:"#{name}[day]", :options => [nil] + 1.upto(31).map(&:to_s), :class => options[:class], :selected => (value.day if value), :disabled => disabled, :required => required)
+      s << text_field_tag(:"#{name}[day]", :class => options[:class], :value => (value.day if value), :disabled => disabled, :required => required)
       s << select_tag(:"#{name}[month]", :options => [nil] + Date::MONTHNAMES[1..-1].each_with_index.map { |x,i| ["#{x}","#{i+1}"] }, :class => options[:class], :selected => (value.month if value), :disabled => disabled, :required => required)
-      s << select_tag(:"#{name}[year]", :options => [nil] + start_year.upto(end_year).map(&:to_s), :class => options[:class], :selected => (value.year if value), :disabled => disabled, :required => required)
+      s << text_field_tag(:"#{name}[year]", :class => options[:class], :value => (value.year if value), :disabled => disabled, :required => required)
       s << '</span>'
       s.join(' ')        
     end

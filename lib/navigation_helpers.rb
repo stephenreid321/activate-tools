0d8950eeaeb0f267065e026d959b00e3ec04eba0
postscript07@gmail.com <postscript07@gmail.com>
2017-10-18 11:50:40 +0100

    .

--- lib/navigation_helpers.rb
+++ lib/navigation_helpers.rb
@@ -1,28 +1,28 @@
 module Activate
   module NavigationHelpers
       
-    def ul_nav(css_class, items, prefix: '', li_class: '', a_class: '')
+    def ul_nav(items, prefix: '', ul_class: '', li_class: '', a_class: '')
       s = ''
-      s << %Q{<ul class="#{css_class}">}      
+      s << %Q{<ul class="#{ul_class}">}      
       items.each { |name, path|
         if path.is_a? Array
           s << %Q{<li class="#{li_class} dropdown">}
           s << %Q{<a data-toggle="dropdown" class="#{a_class} dropdown-toggle" href="javascript:;">#{name} <b class="caret"></b></a>}
-          s << ul_nav('dropdown-menu', path, prefix: prefix, li_class: li_class, a_class: a_class)
+          s << ul_nav('dropdown-menu', path, prefix: prefix, li_class: li_class, a_class: 'dropdown-item')
           s << %Q{</li>}
         elsif path.nil?
           s << %Q{<li role="separator" class="divider"></li>}                          
         else
           path = "#{prefix}#{path}" if prefix
           s << %Q{<li}
-          s << %Q{ class="active #{li_class}" } if request.path == path
+          s << %Q{ class="#{li_class}" }
           s << %Q{>}
-          s << %Q{<a class="#{a_class}" href="#{path}">#{name}</a>}
+          s << %Q{<a class="#{'active' if request.path == path} #{a_class}" href="#{path}">#{name}</a>}
           s << %Q{</li>}
         end
       }
       s << %Q{</ul>}
-    end
+    end  
     
   end
 end
